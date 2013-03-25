c7b4c4f20a8d8778c548db67147fe9cc5b046536
Eloi Perdereau <eloi@perdereau.eu>
2013-03-25 16:48:14 +0100

    modify AdminStatusPanel layout and panels to be able to change between admin and public easily

--- src/main/java/amu/licence/edt/view/AdminStatusPanel.java
+++ src/main/java/amu/licence/edt/view/AdminStatusPanel.java
@@ -1,6 +1,9 @@
 package amu.licence.edt.view;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -11,9 +14,23 @@ import amu.licence.edt.presenter.Presenter;
 
 public class AdminStatusPanel extends ViewComponent {
 
-    private JLabel lblStatus;
-    private JPanel pnlInfos;
+    private static final String CONNECT_BTN = "connect_btn";
+    private static final String DISCONNECT_BTN = "disconnect_btn";
+
+    public final String publicLogin = "utilisateur publique";
+
+    private JLabel lblConnectedAs;
+    private JLabel lblConnexionLogin;
+    private JPanel pnlConnectLbl;
+
+    private JLabel lblManage;
+    private JPanel pnlBtnsManage;
+    private JButton btnManageTeachers;
+    private JPanel pnlManage;
+
     private JButton btnConnect;
+    private JButton btnDisconnect;
+    private JPanel pnlConnectBtn;
 
     public AdminStatusPanel(Presenter presenter) {
         super(presenter);
@@ -23,16 +40,39 @@ public class AdminStatusPanel extends ViewComponent {
     protected JComponent createComponent() {
         JPanel panel = new JPanel();
 
-        lblStatus = new JLabel("unknown");
-        pnlInfos = new JPanel();
+        lblConnectedAs = new JLabel("Connecté en tant que :");
+        lblConnexionLogin = new JLabel(publicLogin);
+        lblConnexionLogin.setForeground(Color.GREEN);
+
+        pnlConnectLbl = new JPanel(new BorderLayout());
+        pnlConnectLbl.add(lblConnectedAs, BorderLayout.NORTH);
+        pnlConnectLbl.add(lblConnexionLogin, BorderLayout.CENTER);
+
+        lblManage = new JLabel("Administration");
+        pnlBtnsManage = new JPanel(new FlowLayout());
+
+        btnManageTeachers = new JButton("Enseignants");
+        btnManageTeachers.addActionListener(null);
+
+        pnlManage = new JPanel(new BorderLayout());
+        pnlManage.add(lblManage, BorderLayout.NORTH);
+        pnlManage.add(pnlBtnsManage, BorderLayout.CENTER);
+        pnlManage.setVisible(false);
 
         btnConnect = new JButton("Connexion");
         btnConnect.addActionListener(null);
 
+        btnDisconnect = new JButton("Déconnexion");
+        btnDisconnect.addActionListener(null);
+
+        pnlConnectBtn = new JPanel(new CardLayout());
+        pnlConnectBtn.add(btnConnect, CONNECT_BTN);
+        pnlConnectBtn.add(btnDisconnect, DISCONNECT_BTN);
+
         panel.setLayout(new BorderLayout());
-        panel.add(lblStatus, BorderLayout.NORTH);
-        panel.add(pnlInfos, BorderLayout.CENTER);
-        panel.add(btnConnect, BorderLayout.SOUTH);
+        panel.add(pnlConnectLbl, BorderLayout.NORTH);
+        panel.add(pnlManage, BorderLayout.CENTER);
+        panel.add(pnlConnectBtn, BorderLayout.SOUTH);
 
         return panel;
     }
