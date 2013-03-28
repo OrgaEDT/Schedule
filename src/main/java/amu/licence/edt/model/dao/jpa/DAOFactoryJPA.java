b5053d6737105c8d24e3aaa4c6e106230d3aebc4
Eloi Perdereau <eloi@perdereau.eu>
2013-03-28 14:17:12 +0100

    fix: remove crap emptySets, commit everything in once

--- src/main/java/amu/licence/edt/model/dao/jpa/DAOFactoryJPA.java
+++ src/main/java/amu/licence/edt/model/dao/jpa/DAOFactoryJPA.java
@@ -2,7 +2,6 @@ package amu.licence.edt.model.dao.jpa;
 
 import java.awt.Color;
 import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -160,12 +159,8 @@ public class DAOFactoryJPA implements DAOFactory {
     public void generateExampleTuples() {
 
         EntityTransaction tr = entityManager.getTransaction();
-
-        Set<Level> emptyLSet = new HashSet<Level>();
-        Set<Teacher> emptyTSet = new HashSet<Teacher>();
-        Set<SessionType> emptySTSet = new HashSet<SessionType>();
-        Set<CRoomType> emptyCRTSet = new HashSet<CRoomType>();
-
+        tr.begin();
+        System.out.println("Generating objects and persisting them...");
 
         /* Ranks */
         System.out.println("Ranks");
@@ -175,14 +170,12 @@ public class DAOFactoryJPA implements DAOFactory {
         Rank r4 = new Rank("PAST");
         Rank r5 = new Rank("ATER");
         Rank r6 = new Rank("Moniteur");
-        tr.begin();
         entityManager.persist(r1);
         entityManager.persist(r2);
         entityManager.persist(r3);
         entityManager.persist(r4);
         entityManager.persist(r5);
         entityManager.persist(r6);
-        tr.commit();
         /* */
 
         /* Levels */
@@ -192,21 +185,19 @@ public class DAOFactoryJPA implements DAOFactory {
         Level l3 = new Level("L3", "Licence 3e année",  null);
         Level m1 = new Level("M1", "Master 1re année",  null);
         Level m2 = new Level("M2", "Master 2e année",   null);
-        tr.begin();
         entityManager.persist(l1);
         entityManager.persist(l2);
         entityManager.persist(l3);
         entityManager.persist(m1);
         entityManager.persist(m2);
-        tr.commit();
         /* */
 
         /* Admins */
         System.out.println("Admins");
-        Admin a1 = new Admin("ladmin", "ladmin", false, emptyLSet);
-        Admin a2 = new Admin("madmin", "madmin", false, emptyLSet);
-        Admin a3 = new Admin("tadmin", "tadmin", true , emptyLSet);
-        Admin a4 = new Admin("aadmin", "aadmin", true , emptyLSet);
+        Admin a1 = new Admin("ladmin", "ladmin", false, new HashSet<Level>());
+        Admin a2 = new Admin("madmin", "madmin", false, new HashSet<Level>());
+        Admin a3 = new Admin("tadmin", "tadmin", true , new HashSet<Level>());
+        Admin a4 = new Admin("aadmin", "aadmin", true , new HashSet<Level>());
         a1.addLevel(l1);
         a1.addLevel(l2);
         a1.addLevel(l3);
@@ -217,28 +208,25 @@ public class DAOFactoryJPA implements DAOFactory {
         a4.addLevel(l3);
         a4.addLevel(m1);
         a4.addLevel(m2);
-        tr.begin();
         entityManager.persist(a1);
         entityManager.persist(a2);
         entityManager.persist(a3);
         entityManager.persist(a4);
-        tr.commit();
         /* */
 
         /* Teachers */
         System.out.println("Teachers");
-        Teacher talbot   = new Teacher("talbot",   "talbot",   false, emptyLSet, "Jean-Marc TALBOT",        "Luminy",    null, null, 0, r3);
-        Teacher massat   = new Teacher("massat",   "massat",   false, emptyLSet, "Jean-Luc MASSAT",         "Luminy",    null, null, 0, r3);
-        Teacher jamet    = new Teacher("jamet",    "jamet",    false, emptyLSet, "Line JAKUBIEC-JAMET",     "Luminy",    null, null, 0, r2);
-        Teacher sabatier = new Teacher("sabatier", "sabatier", false, emptyLSet, "Claude SABATIER",         "Luminy",    null, null, 0, r2);
-        Teacher chepoi   = new Teacher("chepoi",   "chepoi",   false, emptyLSet, "Victor CHEPOI",           "Luminy",    null, null, 0, r2);
-        Teacher nouioua  = new Teacher("nouioua",  "nouioua",  false, emptyLSet, "Karim NOUIOUA",           "Luminy",    null, null, 0, r3);
-        Teacher nasr     = new Teacher("nasr",     "nasr",     false, emptyLSet, "Alexis NASR",             "Luminy",    null, null, 0, r2);
-        Teacher bechet   = new Teacher("bechet",   "bechet",   false, emptyLSet, "Frederique BECHET",       "Luminy",    null, null, 0, r3);
-        Teacher estellon = new Teacher("estellon", "estellon", false, emptyLSet, "Bertrand ESTELLON",       "Luminy",    null, null, 0, r2);
-        Teacher ritz     = new Teacher("ritz",     "ritz",     false, emptyLSet, "Christophe RITZENTHALER", "Extérieur", null, null, 0, r2);
-        Teacher avell    = new Teacher("avell",    "avell",    false, emptyLSet, "Florent AVELLANEDA",      "Extérieur", null, null, 0, r6);
-        tr.begin();
+        Teacher talbot   = new Teacher("talbot",   "talbot",   false, new HashSet<Level>(), "Jean-Marc TALBOT",        "Luminy",    null, null, 0, r3);
+        Teacher massat   = new Teacher("massat",   "massat",   false, new HashSet<Level>(), "Jean-Luc MASSAT",         "Luminy",    null, null, 0, r3);
+        Teacher jamet    = new Teacher("jamet",    "jamet",    false, new HashSet<Level>(), "Line JAKUBIEC-JAMET",     "Luminy",    null, null, 0, r2);
+        Teacher sabatier = new Teacher("sabatier", "sabatier", false, new HashSet<Level>(), "Claude SABATIER",         "Luminy",    null, null, 0, r2);
+        Teacher chepoi   = new Teacher("chepoi",   "chepoi",   false, new HashSet<Level>(), "Victor CHEPOI",           "Luminy",    null, null, 0, r2);
+        Teacher nouioua  = new Teacher("nouioua",  "nouioua",  false, new HashSet<Level>(), "Karim NOUIOUA",           "Luminy",    null, null, 0, r3);
+        Teacher nasr     = new Teacher("nasr",     "nasr",     false, new HashSet<Level>(), "Alexis NASR",             "Luminy",    null, null, 0, r2);
+        Teacher bechet   = new Teacher("bechet",   "bechet",   false, new HashSet<Level>(), "Frederique BECHET",       "Luminy",    null, null, 0, r3);
+        Teacher estellon = new Teacher("estellon", "estellon", false, new HashSet<Level>(), "Bertrand ESTELLON",       "Luminy",    null, null, 0, r2);
+        Teacher ritz     = new Teacher("ritz",     "ritz",     false, new HashSet<Level>(), "Christophe RITZENTHALER", "Extérieur", null, null, 0, r2);
+        Teacher avell    = new Teacher("avell",    "avell",    false, new HashSet<Level>(), "Florent AVELLANEDA",      "Extérieur", null, null, 0, r6);
         entityManager.persist(talbot);
         entityManager.persist(massat);
         entityManager.persist(jamet);
@@ -250,15 +238,12 @@ public class DAOFactoryJPA implements DAOFactory {
         entityManager.persist(estellon);
         entityManager.persist(ritz);
         entityManager.persist(avell);
-        tr.commit();
         /* */
 
         /* Promos */
         System.out.println("Promos");
         Promo p1 = new Promo(60, l3, new HashSet<Group>());
-        tr.begin();
         entityManager.persist(p1);
-        tr.commit();
         /* */
 
         /* Groups */
@@ -267,23 +252,21 @@ public class DAOFactoryJPA implements DAOFactory {
         Group g2 = new Group(30, p1);
         p1.addGroup(g1);
         p1.addGroup(g2);
-        tr.begin();
         entityManager.persist(g1);
         entityManager.persist(g2);
-        tr.commit();
         /* */
 
         /* TUs */
         System.out.println("TUs");
-        TU compil  = new TU("ENSIN6U1", "Compilation",                            Color.getColor("#CC9933"), 20, 20, 20, 0  , l3, nasr,     emptyTSet);
-        TU logique = new TU("ENSIN6U2", "Logique et calculabilité",               Color.getColor("#FF9900"), 20, 30, 0 , 0  , l3, jamet,    emptyTSet);
-        TU syst    = new TU("ENSIN6U3", "Systèmes d'exploitation",                Color.getColor("#FFFF99"), 20, 20, 20, 0  , l3, massat,   emptyTSet);
-        TU geoAlgo = new TU("ENSIN6U4", "Géométrie algorithmique",                Color.getColor("#CC6666"), 56, 0 , 20, 0  , l3, chepoi,   emptyTSet);
-        TU devWeb  = new TU("ENSIN6U5", "Développement web 2",                    Color.getColor("#6699FF"), 28, 0 , 56, 0  , l3, estellon, emptyTSet);
-        TU crypto  = new TU("ENSMI6U6", "Cryptographie",                          Color.getColor("#33FFFF"), 28, 26, 24, 0  , l3, ritz,     emptyTSet);
-        TU tal     = new TU("ENSMI6U7", "Langage naturel",                        Color.getColor("#66FFFF"), 0 , 0 , 0 , 0  , l3, nasr,     emptyTSet);
-        TU pPOO    = new TU("ENSIN6U8", "Projet de Programmation Orientée Objet", Color.getColor("#00CC99"), 4 , 0 , 0 , 100, l3, avell,    emptyTSet);
-        TU pApp    = new TU("ENSIN6U9", "Projet informatique appliqué",           Color.getColor("#00CC99"), 6 , 0 , 0 , 80 , l3, sabatier, emptyTSet);
+        TU compil  = new TU("ENSIN6U1", "Compilation",                            Color.getColor("#CC9933"), 20, 20, 20, 0  , l3, nasr,     new HashSet<Teacher>());
+        TU logique = new TU("ENSIN6U2", "Logique et calculabilité",               Color.getColor("#FF9900"), 20, 30, 0 , 0  , l3, jamet,    new HashSet<Teacher>());
+        TU syst    = new TU("ENSIN6U3", "Systèmes d'exploitation",                Color.getColor("#FFFF99"), 20, 20, 20, 0  , l3, massat,   new HashSet<Teacher>());
+        TU geoAlgo = new TU("ENSIN6U4", "Géométrie algorithmique",                Color.getColor("#CC6666"), 56, 0 , 20, 0  , l3, chepoi,   new HashSet<Teacher>());
+        TU devWeb  = new TU("ENSIN6U5", "Développement web 2",                    Color.getColor("#6699FF"), 28, 0 , 56, 0  , l3, estellon, new HashSet<Teacher>());
+        TU crypto  = new TU("ENSMI6U6", "Cryptographie",                          Color.getColor("#33FFFF"), 28, 26, 24, 0  , l3, ritz,     new HashSet<Teacher>());
+        TU tal     = new TU("ENSMI6U7", "Langage naturel",                        Color.getColor("#66FFFF"), 0 , 0 , 0 , 0  , l3, nasr,     new HashSet<Teacher>());
+        TU pPOO    = new TU("ENSIN6U8", "Projet de Programmation Orientée Objet", Color.getColor("#00CC99"), 4 , 0 , 0 , 100, l3, avell,    new HashSet<Teacher>());
+        TU pApp    = new TU("ENSIN6U9", "Projet informatique appliqué",           Color.getColor("#00CC99"), 6 , 0 , 0 , 80 , l3, sabatier, new HashSet<Teacher>());
         compil.addCompetentTeacher(nasr);
         compil.addCompetentTeacher(bechet);
         logique.addCompetentTeacher(jamet);
@@ -299,7 +282,6 @@ public class DAOFactoryJPA implements DAOFactory {
         pPOO.addCompetentTeacher(avell);
         pApp.addCompetentTeacher(jamet);
         pApp.addCompetentTeacher(sabatier);
-        tr.begin();
         entityManager.persist(compil);
         entityManager.persist(logique);
         entityManager.persist(syst);
@@ -309,28 +291,29 @@ public class DAOFactoryJPA implements DAOFactory {
         entityManager.persist(tal);
         entityManager.persist(pPOO);
         entityManager.persist(pApp);
-        tr.commit();
         /* */
+
+        for (Teacher t : syst.getCompetentTeachers()) {
+            System.out.println(t.getName());
+        }
 
         /* SessionTypes */
         System.out.println("SessionTypes");
-        SessionType sessionTD = new SessionType("TD", 1, emptyCRTSet);
-        SessionType sessionTP = new SessionType("TP", 1, emptyCRTSet);
-        SessionType sessionCours = new SessionType("Lecture", 1, emptyCRTSet);
-        SessionType sessionProjet = new SessionType("Project", 1, emptyCRTSet);
-        tr.begin();
+        SessionType sessionTD = new SessionType("TD", 1, new HashSet<CRoomType>());
+        SessionType sessionTP = new SessionType("TP", 1, new HashSet<CRoomType>());
+        SessionType sessionCours = new SessionType("Lecture", 1, new HashSet<CRoomType>());
+        SessionType sessionProjet = new SessionType("Project", 1, new HashSet<CRoomType>());
         entityManager.persist(sessionTD);
         entityManager.persist(sessionTP);
         entityManager.persist(sessionCours);
         entityManager.persist(sessionProjet);
-        tr.commit();
         /* */
 
         /* CRoomTypes */
         System.out.println("CRoomTypes");
-        CRoomType salleTD = new CRoomType("TD", emptySTSet);
-        CRoomType salleTP = new CRoomType("TP", emptySTSet);
-        CRoomType amphi   = new CRoomType("Amphi", emptySTSet);
+        CRoomType salleTD = new CRoomType("TD", new HashSet<SessionType>());
+        CRoomType salleTP = new CRoomType("TP", new HashSet<SessionType>());
+        CRoomType amphi   = new CRoomType("Amphi", new HashSet<SessionType>());
         salleTD.addCompatibleSessionType(sessionTD);
         salleTD.addCompatibleSessionType(sessionCours);
         salleTD.addCompatibleSessionType(sessionProjet);
@@ -338,11 +321,9 @@ public class DAOFactoryJPA implements DAOFactory {
         salleTP.addCompatibleSessionType(sessionProjet);
         amphi.addCompatibleSessionType(sessionCours);
         amphi.addCompatibleSessionType(sessionTD);
-        tr.begin();
         entityManager.persist(salleTD);
         entityManager.persist(salleTP);
         entityManager.persist(amphi);
-        tr.commit();
         /* */
 
         /* CRooms */
@@ -396,7 +377,6 @@ public class DAOFactoryJPA implements DAOFactory {
         CRoom cr47 = new CRoom("0.03B",    25, salleTP);
         CRoom cr48 = new CRoom("0.04B",    25, salleTP);
         CRoom cr49 = new CRoom("0.05B",    25, salleTP);
-        tr.begin();
         entityManager.persist(cr1 );
         entityManager.persist(cr2 );
         entityManager.persist(cr3 );
@@ -446,12 +426,16 @@ public class DAOFactoryJPA implements DAOFactory {
         entityManager.persist(cr47);
         entityManager.persist(cr48);
         entityManager.persist(cr49);
-        tr.commit();
         /* */
 
         /* Sessions */
         // TODO
         /* */
+
+        System.out.println("Committing...");
+        tr.commit();
+//        tr.rollback();
+        System.out.println("Done");
 
     }
 
