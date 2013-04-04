3b6459725819ec13aff9ee8bc429dab7cd72d9cd
Eloi Perdereau <eloi@perdereau.eu>
2013-04-04 13:11:30 +0200

    change group display: add 'G' cat id

--- src/main/java/amu/licence/edt/view/renderers/GroupRenderer.java
+++ src/main/java/amu/licence/edt/view/renderers/GroupRenderer.java
@@ -8,7 +8,7 @@ public class GroupRenderer implements Renderer {
     public String getStrRender(Object obj) {
         try {
             Group group = (Group) obj;
-            return group.getId()+"";   // maby find a better display
+            return "G"+group.getId();   // maby find a better display
         } catch (ClassCastException cce) {
             System.err.println("error: trying to render a non-Group in GroupRenderer");
             return null;
