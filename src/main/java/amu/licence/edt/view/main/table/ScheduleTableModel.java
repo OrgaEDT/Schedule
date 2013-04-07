35e87f56111f263373b8a6f19e698ab611685649
Eloi Perdereau <eloi@perdereau.eu>
2013-04-07 20:05:01 +0200

    add debug tests on sched table model

--- src/main/java/amu/licence/edt/view/main/table/ScheduleTableModel.java
+++ src/main/java/amu/licence/edt/view/main/table/ScheduleTableModel.java
@@ -11,15 +11,21 @@ public class ScheduleTableModel extends AbstractTableModel implements SpanTableM
 
     private SpanModel spanModel = new SpanModel();
     private final TU[][] donnees;
-    private final String[] entete = { "matin", "midi", "soir" };
+    private final String[] entete = { "8h", "10h", "12h", "14h", "16h", "18h" };
 
     public ScheduleTableModel() {
         super();
         donnees = new TU[][] {
                 { new TU("ue1", Color.decode("#BBBBBB")),
+                        new TU("ue2", Color.RED), new TU("ue3", Color.BLUE),
+                        new TU("ue1", Color.decode("#BBBBBB")),
                         new TU("ue2", Color.RED), new TU("ue3", Color.BLUE) },
                 { new TU("ue4", Color.YELLOW), new TU("ue5", Color.GRAY),
+                        new TU("ue6", Color.GREEN),
+                        new TU("ue4", Color.YELLOW), new TU("ue5", Color.GRAY),
                         new TU("ue6", Color.GREEN) }, };
+        spanModel.addSpan(new Span(0, 1, 4));
+        spanModel.addSpan(new Span(1, 3, 2));
     }
 
     @Override
